b1d0232a39af79f2c62e7163175ce58e02021c66
AmaraABC <amara-noah.gakou@efrei.net>
2025-02-09 14:06:10 +0100

    Updating main.ts file: addition of a new function, 'fetchAPI'

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+// Importation du client HTTP Axios
+import axios from 'axios';
+
+// Fonction qui interroge l'API SpaceX
+async function fetchAPI() {
+  // Tentative d'interaction avec l'API SpaceX V5
+    try {
+    const response = await axios.get('https://api.spacexdata.com/v5/launches');
+    return response.data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des lancements de l\'entreprise SpaceX:', error);
+  }
+}
+
 createApp(App).mount('#app')
