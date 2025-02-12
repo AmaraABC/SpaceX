e090370f1c8dbf6d80130db2ef88c571765a1b81
AmaraABC <amara-noah.gakou@efrei.net>
2025-02-12 09:37:06 +0100

    Updated version of this file: 2 new functions and 1 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
 // Importation du client HTTP Axios
 import axios from 'axios';
 
-// Fonction qui interroge l'API SpaceX
-async function fetchAPI() {
-  // Tentative d'interaction avec l'API SpaceX v5
-    try {
-    const response = await axios.get('https://api.spacexdata.com/v5/launches');
-    return response.data;
+// Le lien de l'API
+const SpaceXAPI = 'https://api.spacexdata.com/v5/launches';
+
+// Fonction qui interroge l'API SpaceX afin de récupérer tous les lancements
+export const getAllLaunches = async (filter: string) => {
+  try {
+    const res = await axios.get(`${SpaceXAPI}?filter=${filter}`);
+    return res.data;
   } catch (error) {
     console.error('Erreur lors de la récupération des lancements de l\'entreprise SpaceX:', error);
+    return [];
   }
-}
-createApp(App).mount('#app')
+};
+
+// Fonction qui interroge l'API SpaceX pour récupérer le prochain lancement
+export const getNextLaunch = async () => {
+  try {
+    const res = await axios.get(`${SpaceXAPI}/next`);
+    return res.data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération des lancements de l\'entreprise SpaceX:', error);
+    return null;
+  }
+};
